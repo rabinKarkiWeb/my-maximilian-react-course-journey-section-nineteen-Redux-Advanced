aa936d880d48d142e31d381028ee1f00ced9a25b
Rabin Karki <rabinkarkiweb@gmail.com>
2022-11-06 21:55:50 +0545

    cart-slice added ui-slice completed using redux toolkit

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,14 @@
 import classes from './CartButton.module.css';
+import {uiActions} from "../store/ui-slice";
+import {useDispatch} from "react-redux";
 
 const CartButton = (props) => {
+    const dispatch = useDispatch();
+    const toggleCartHandler =()=>{
+        dispatch(uiActions.toggle());
+    }
   return (
-    <button className={classes.button}>
+    <button onClick={toggleCartHandler} className={classes.button}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
